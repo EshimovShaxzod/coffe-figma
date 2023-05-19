008289babf57c161e18a73e5ff2c613102e25432
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2023-05-19 12:25:44 +0500

    coffe figma

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let shaxzod = document.getElementById('openMenu');
+let elHeaderMenu = document.getElementById("navList");
+let elIcon = document.getElementById('icon')
+
+let count = 0;
+shaxzod.addEventListener('click', function()
+{
+  elHeaderMenu.classList.toggle('show');
+  if (count == 0){
+    shaxzod.style.color = 'red';
+    elIcon.className = "bx bx-x";
+    count++
+  }else {
+    shaxzod.style.color = '#000';
+    elIcon.className = "bx bx-menu";
+    count = 0;
+  }
+})
